425e5512812f358618a6060bc238ed227f5f9d8c
LeoKim <leo3179@naver.com>
2022-05-18 17:01:52 +0900

    💚 cloudbuild check

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './commons/filter/http-exception-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+  });
   app.use(graphqlUploadExpress());
   await app.listen(3000);
 }

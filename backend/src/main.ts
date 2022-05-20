2c8ce43bec7254f86ab0b023971cbe3a21dcb7e6
고재형 <jaehyeongko@baeg-endeujangkkusseu.local>
2022-05-20 19:43:09 +0900

    :sparkles: 실시간 채팅구현#1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { graphqlUploadExpress } from 'graphql-upload';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './commons/filter/http-exception-filter';
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule); // NestApplication 소켓 통신을 위해 설정
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors({
     credentials: true,
     origin: 'http://localhost:3000',
   });
+  app.useStaticAssets(join(__dirname, '..', 'static')) // main.ts 채팅 설정(NestApplication)
   app.use(graphqlUploadExpress());
   await app.listen(3000);
 }

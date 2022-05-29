88b4989a29ad00ba33694d27d69b8480d40034e0
LeoKim <leo3179@naver.com>
2022-05-29 15:14:41 +0900

    🎨 chat.module 수정

--- backend/src/apis/chat/chat.module.ts
+++ backend/src/apis/chat/chat.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
 import { ChatGateway } from './chat.gateway';
+import { AppGateway } from './chat.gateway1';
 import { ChatResolver } from './chat.resolver';
 import { ChatService } from './chat.service';
 // import { ChatResolver } from './chat.resolver';
@@ -9,12 +10,7 @@ import { ChatService } from './chat.service';
 import { Chat } from './entities/chat.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([
-      Chat,
-      User
-    ])
-  ],
-  providers: [ ChatGateway, ChatResolver, ChatService ],
+  imports: [TypeOrmModule.forFeature([Chat, User])],
+  providers: [AppGateway, ChatResolver, ChatService],
 })
 export class ChatModule {}
